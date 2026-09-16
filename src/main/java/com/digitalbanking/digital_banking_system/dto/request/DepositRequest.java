3ddc914b8013cc6a4fb6de4d15cc57f70e036e3f
PH31M <ducminh03102005@gmail.com>
2026-09-16 11:49:03 +0700

    create request v1 refactor transaction controller

---
+++ src/main/java/com/digitalbanking/digital_banking_system/dto/request/DepositRequest.java
@@ -0,0 +1,29 @@
+package com.digitalbanking.digital_banking_system.dto.request;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepositRequest {
+
+    @NotBlank(message = "Account number is required")
+    private String accountNumber;
+
+    @NotNull(message = "Amount is required")
+    @DecimalMin(value = "0.01", message = "Deposit amount must be greater than zero")
+    private BigDecimal amount;
+
+    @Size(max = 500, message = "Description cannot exceed 500 characters")
+    private String description;
+}
